870b9de92930d47298064d49040a9d0b38db2feb
Jabir Bhuiyan <jabirbhuiyan08@gmail.com>
2025-04-26 04:14:33 +0800

    Scroll to Top added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import ScrollToTop from "../Components/ScrollToTop";
 
 const Main = () => {
   return (
     <div>
+      <ScrollToTop></ScrollToTop>
       <div className="">
 
       <Navbar></Navbar>
       </div>
       <div className="max-w-screen-xl mx-auto px-4 py-6 mt-32">
+        
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Router } from "./router/router.jsx";
 import AuthProvider from "./Provider/AuthProvider.jsx";
 
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -16,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <div className="w-full min-h-screen bg-white">
-          <RouterProvider router={Router} />
+          <RouterProvider router={Router} /> {/* No ScrollToTop here */}
         </div>
       </AuthProvider>
     </QueryClientProvider>
