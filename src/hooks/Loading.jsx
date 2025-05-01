786e514c2cc3a38304976e9700d3654a3747f573
Jabir Bhuiyan <jabirbhuiyan08@gmail.com>
2025-05-01 13:56:36 +0800

    loading design

--- src/hooks/Loading.jsx
+++ src/hooks/Loading.jsx
@@ -3,12 +3,13 @@ import useAuth from './useAuth';
 import logo from '../assets/logo-2.png'
 
 const Loading = () => {
-    const {Loading} = useAuth();
-    if (Loading) {
+    const {loading} = useAuth();
+    if (loading) {
         return (
             <div className='flex flex-col justify-center items-center h-screen '>
                 <img src={logo} alt="" className='w-32'/>
                 <span className="loading loading-infinity loading-xl text-blue-400"></span>
+            <h1 className='text-xl'>Loading...</h1>
             </div>
         )
     }
